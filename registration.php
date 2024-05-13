61e497791f0a487b345d757632d5c40243789912
Szymon Borowski <szymon.borowski@gmail.com>
2024-05-13 19:14:54 +0200

    Module skeleton

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author Szymon Borowski <szymon.borowski@gmail.com>
+ * @copyright Copyright (c) 2024, Szymon Borowski
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Szybo_Pokemon',
+    __DIR__
+);
